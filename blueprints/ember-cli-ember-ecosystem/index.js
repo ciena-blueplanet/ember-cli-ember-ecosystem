0af4f2978a5f9d2c5e31bf52db59fbc93a63be74
David Fortin <dafortin@ciena.com>
2016-09-16 11:34:59 -0400

    Fix linting issues

--- blueprints/ember-cli-ember-ecosystem/index.js
+++ blueprints/ember-cli-ember-ecosystem/index.js
@@ -1,15 +1,3 @@
-/*jshint node:true*/
 module.exports = {
   description: ''
-
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
-};
+}
