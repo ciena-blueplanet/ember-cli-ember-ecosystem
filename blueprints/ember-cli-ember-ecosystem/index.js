c45dca325835b1d97cf4a15d288e854723025538
David Fortin <dafortin@ciena.com>
2016-09-16 14:20:00 -0400

    Add lts file and ecosystem installer in blueprint

--- blueprints/ember-cli-ember-ecosystem/index.js
+++ blueprints/ember-cli-ember-ecosystem/index.js
@@ -1,3 +1,11 @@
 module.exports = {
-  description: ''
+  description: '',
+  normalizeEntityName: function () {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+  afterInstall: function () {
+    return this.addAddonsToProject({ packages: [{name: 'ember-cli-ecosystem-installer'}] })
+  }
 }
